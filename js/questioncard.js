93f8e95adac5d2283810fca2b8925f0add90241b
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-23 16:16:12 +0100

    create js component for questioncard

---
+++ js/questioncard.js
@@ -0,0 +1,46 @@
+function createQuestionCard() {
+  const main = document.querySelector('[data-js="main"]');
+
+  // Create the outer section
+  const sectionCard = document.createElement("section");
+  sectionCard.classList.add("questioncard");
+  main.insertAdjacentElement("afterbegin", sectionCard); // Fix: insert sectionCard into main
+
+  // Create the Bookmark Button
+  const buttonBookmark = document.createElement("button");
+  buttonBookmark.classList.add("button--bookmark");
+  sectionCard.appendChild(buttonBookmark);
+
+  // Create a span into Bookmark Button
+  const iconBookmark = document.createElement("span");
+  iconBookmark.classList.add("material-symbols-outlined", "card--icon"); // Fix: separate class names
+  iconBookmark.textContent = "bookmark"; // Fix: add text content to span
+  buttonBookmark.appendChild(iconBookmark);
+
+  // Create Card Question
+  const cardQuestion = document.createElement("h3");
+  cardQuestion.classList.add("card--title");
+  cardQuestion.textContent = "Question for card";
+  sectionCard.appendChild(cardQuestion);
+
+  // Create the Answer Button
+  const answerButton = document.createElement("button");
+  answerButton.classList.add("button--answer");
+  answerButton.textContent = "show answer";
+  sectionCard.appendChild(answerButton);
+
+  // Create Taglist
+  const tagList = document.createElement("ul");
+  tagList.classList.add("tagline");
+  sectionCard.appendChild(tagList);
+
+  // Create Tags in Taglist
+  const tag = document.createElement("li");
+  tag.classList.add("tag");
+  tag.textContent = "#tag";
+  tagList.appendChild(tag);
+
+  console.log("HelloHello");
+}
+
+createQuestionCard();

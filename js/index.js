1e985d144ea0292dfd2a8a0038595f81bf95bacb
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-23 20:05:05 +0100

    delete broken function

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// Save to Bookmarks Click Event
-
-const saveToBookmarksButton = document.querySelector('[data-js="bookmark"]');
-const paintBookmark = document.querySelector('[data-js="bookmark--icon"]');
-
-saveToBookmarksButton.addEventListener("click", () => {
-  paintBookmark.classList.toggle("card--icon--active");
-});
